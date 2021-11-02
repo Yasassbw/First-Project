68d83e0aced0c7add5b61cb00bf4ca889225e951
Kavinda Harshana <kavinda@silverstripers.com>
2021-11-02 14:30:33 +0530

    students list

--- functions.php
+++ functions.php
@@ -26,3 +26,18 @@ function getCurrentPage()
 {
     return substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
 }
+
+function getStudentsList($con)
+{
+    $data = [];
+    $query = "SELECT * FROM students";
+    $result = mysqli_query($con, $query);
+    if ($result && mysqli_num_rows($result) > 0) {
+        while($row = $result->fetch_assoc()) {
+            array_push($data, $row);
+        }
+        return $data;
+    }else {
+        return [];
+    }
+}
--- studentsList.php
+++ studentsList.php
@@ -1,20 +1,46 @@
 <?php
-
+$conn = OpenCon();
+$studentsList = getStudentsList($conn);
 ?>
 <div class="container">
     <a href="studentRegistration.php">Add Student</a><br>
     <ul class="list-group">
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            Inbox
-            <span class="badge badge-primary badge-pill">12</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            Ads
-            <span class="badge badge-primary badge-pill">50</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            Junk
-            <span class="badge badge-primary badge-pill">99</span>
-        </li>
+
+        <table class="table table-dark">
+            <thead>
+
+            <?php
+            if (is_array($studentsList) && count($studentsList)) {
+
+                echo ' <tr>
+                <th scope="col">Photo</th>
+                <th scope="col">Student ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Address</th>
+                <th scope="col">Course ID</th>
+            </tr>
+            </thead>
+            <tbody>';
+
+                foreach ($studentsList as $item => $value) {
+                    echo '
+        <tr>
+                <th scope="row"><img src="" width="80px" height="80px"></th>
+                <td>' . $value['student_id'] . '</td>
+                <td>' . $value['name'] . '</td>
+                <td>' . $value['email'] . '</td>
+                <td>' . $value['address'] . '</td>
+                <td>' . $value['course_id'] . '</td>
+            </tr>
+        ';
+                }
+            }else {
+                echo '<p class="list-group-item d-flex justify-content-between align-items-center">No items found</p>';
+            }
+
+            ?>
+            </tbody>
+        </table>
     </ul>
 </div>
